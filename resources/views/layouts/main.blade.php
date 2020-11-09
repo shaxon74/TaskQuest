27cafdf1e0792b6ad7261a1b78544be219d62612
水口一成 <shaxon74@gmail.com>
2020-11-09 03:13:50 +0000

    css修正

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,8 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ja">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <title>@yield('title')</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -19,13 +10,16 @@
 
 <body>
     @include('include.header')
-    @auth
-        @include('include.sidebar')
-    @endauth
-    <!-- コンテンツ -->
-    <div class="content">
-        @yield('content')
+    <div class="wrapper">
+        <!-- コンテンツ -->
+        <div class="content">
+            @yield('content')
+        </div>
+        @auth
+            @include('include.sidebar')
+        @endauth
     </div>
     <script type="text/javascript" src="{{ mix('js/app.js') }}" ></script>
     @include('include.footer')
 </body>
+</html>
