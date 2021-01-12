458b7719054bc67bf58cede7e036fdd3a351f4e2
水口一成 <shaxon74@gmail.com>
2021-01-12 22:28:20 +0000

    サイドメニューCSS更新(作りかけ)

--- resources/views/include/sidemenu.blade.php
+++ resources/views/include/sidemenu.blade.php
@@ -5,9 +5,24 @@
             <h><a href="/tasks">タスク詳細一覧</a></h>
         </div>
     @else
-    <div class="content">
-        <p>※アプリ説明、ガイダンスを記載</p>
-        <p><a href="/#">まずは登録！</a></p>
-    </div>
+    <h2 class="heading">RANKING</h2>
+    <ol class="ranking">
+        <li class="ranking-item">
+            <a href="#">
+                <img class="image" src="./images/ranking.jpg" alt="グラフの画像" />
+                <span class="text"></span>
+                <p>HTML/CSSコーディングと切っても切れないWEBブラウザの
+                シェア動向をチェックしよう</p>
+            </a>
+        </li>
+        <li class="ranking-item">
+            <a href="#">
+                <img class="image" src="./images/ranking.jpg" alt="グラフの画像" />
+                <span class="text"></span>
+                <p>HTML/CSSコーディングと切っても切れないWEBブラウザの
+                シェア動向をチェックしよう</p>
+            </a>
+        </li>
+    </ol>
     @endauth
 </div>
