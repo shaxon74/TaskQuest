34a2e5bfc9294152d1da854f6cb5305dfff8d94b
Kazunari Mizuguchi <mizuguchi.kazunari@alpha-tec.co.jp>
2021-01-29 18:02:46 +0900

    タスク情報からモンスター一覧を作成(途中)。

--- resources/js/components/user/DayComponent.vue
+++ resources/js/components/user/DayComponent.vue
@@ -3,10 +3,12 @@
         <div class="date">
             <monster-component></monster-component>
         </div>
-        <button v-on:click="monsters">test</button>
         <ul class="dbg">
-            <li v-for="task in tasks">{{ task }}</li>
+            <li v-for="monster in monsters">{{ monster.name }}</li>
         </ul>
+        <!-- <ul class="dbg">
+            <li v-for="task in tasks">{{ task }}</li>
+        </ul> -->
     </div>
 </template>
 
@@ -14,65 +16,56 @@
 export default {
     data: function(){
         return {
-            tasks: [],
-            days_length: 10
+            days_length: 10,
+            monsters: []
         }
     },
     mounted: function(){
-        axios.get('/tasks_axios').then(response => this.tasks = response.data)
+        this.create_monsters();
     },
     methods: {
-        next_weekend: function(){
-            console.log("hello!");
-        },
-        monsters: function(tasks){
-            //日付処理が余りにもクソ面倒(月跨ぎのときは特に)
-            // ->day.jsとかいうjsのライブラリが便利そう
-            var dayjs = require('dayjs');
-            var today = dayjs();
-            var monsters = [];
-            var date_current = dayjs();
-            var date_limit = dayjs();
-            var monster = {};
-            // console.log( 'today: ' + today.format('YYYY/MM/DD') );
-            // console.log('days_length: ' + this.days_length);
-            // console.log(this.tasks);
-            for(var cnt = 0; cnt < this.days_length; cnt++) {
-                date_current = today.add(cnt, 'day');
-                // console.log( 'date_current: ' + date_current.format('YYYY/MM/DD') );
-                this.tasks.forEach(function( task ) {
-                    // console.log( 'task.type: ' + task.type );
-                    switch(task.type){
-                        case 1:
-                            console.log( 'task.type: ' + task.type );
+        create_monsters: function(){
+            // Axiosでレコードを取出す。
+            axios.get('/tasks_axios').then(response => {
+                var tasks = response.data;
+                console.log(tasks);
+                var dayjs = require('dayjs');   // day.jsライブラリの呼出し
+                var today = dayjs();
+                var date_current = dayjs();     // for文でターゲットとしている日付
+                var date_limit = dayjs();       // task.typeから判定したタスク期限
+                // 表示する日付長だけmonsterデータを作成する。
+                for(var cnt = 0; cnt < this.days_length; cnt++) {
+                    date_current = today.add(cnt, 'day');
+                    tasks.forEach(function( task ) {
+                        switch(task.type){
+                            case 1:
                             date_limit = date_current;
                             break;
-                        case 2:
-                            console.log( 'task.type: ' + task.type );
+                            case 2:
                             date_current.day() == 0 ?
-                                date_limit = date_current :
-                                date_limit = date_current.add( 7 - date_current.day(), 'day' );
+                            date_limit = date_current :
+                            date_limit = date_current.add( 7 - date_current.day(), 'day' );
                             break;
-                        case 3:
-                            console.log( 'task.type: ' + task.type );
+                            case 3:
                             date_limit = date_current.date(1).add(1, 'month' ).subtract(1, 'day');
                             break;
-                    }
-                    console.log( 'date_limit: ' + date_limit.format('YYYY/MM/DD') );
-                    if( date_limit.isSame(date_current) ){
-                        monster.name       = task.name;
-                        monster.date_limit = date_limit;
-                        monster.reword     = task.reword;
-                        console.log(monster.name);
-                        console.log(monster.date_limit);
-                        console.log(monster.reword);
-                        monsters.push(monster);
-                        // console.log(dayjs(monster.date_limit).format('YYYY/MM/DD'));
-                    }
-                });
-            }
-            console.log(monsters);
-        }
+                        }
+                        // taskからmonster連想配列を生成しmonsters配列にpushする。
+                        if( date_limit.isSame(date_current) ){
+                            console.log(task.name);
+                            console.log(date_limit);
+                            console.log(task.reword);
+                            this.monsters.push({
+                                'name':       task.name,
+                                'date_limit': date_limit,
+                                'reword':     task.reword});
+                        }   // endif
+                    }); // endforeach
+                }   // endfor
+                console.log('Monsters created!');
+                console.log(this.monsters);
+            });
+        } // endfunction
     }
 }
 </script>
