62415c3b18cdb5db43f7d8960e9f5e3fa0c7f8cf
Kazunari Mizuguchi <shaxon74@gmail.com>
2021-03-16 17:58:34 +0900

    タスク編集用vueファイルを作成。

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,14 +22,15 @@ window.Vue = require('vue');
 // モンスター表示コンポーネント
 Vue.component('example-component',      require('./components/ExampleComponent.vue').default);
 Vue.component('original-component',     require('./components/OriginalComponent.vue').default);
-Vue.component('main-component',        require('./components/user/MainComponent.vue').default);
+Vue.component('main-component',         require('./components/user/MainComponent.vue').default);
 Vue.component('taskmonsters-component', require('./components/user/TaskMonstersComponent.vue').default);
 Vue.component('monster-component',      require('./components/user/MonsterComponent.vue').default);
 Vue.component('mymenu-component',       require('./components/user/MyMenuComponent.vue').default);
 Vue.component('monstermenu-component',  require('./components/user/MonsterMenuComponent.vue').default);
 
 // タスク管理コンポーネント
-Vue.component('tasks-component',        require('./components/user/TasksComponent.vue').default);
+Vue.component('tasks-component',     require('./components/user/TasksComponent.vue').default);
+Vue.component('edittasks-component', require('./components/user/EditTasksComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ resources/js/components/user/EditTasksComponent.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="edit-tasks">
+    <h2>タスク一覧</h2>
+    <table class="task-list">
+        <tr>
+            <th>name</th>
+            <th>type</th>
+            <th>reword</th>
+            <th>is_finished</th>
+        </tr>
+        <tr>
+            <td>name</td>
+            <td>type</td>
+            <td>reword</td>
+            <td>finished</td>
+        </tr>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            tasks: []
+        }
+    },
+    methods: {
+        getTasks: function() {
+            let tasks;
+            axios.get('/tasks').then(response => {
+                tasks = response.data.tasks;
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
